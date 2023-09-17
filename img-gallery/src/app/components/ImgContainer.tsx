eaaa1818809a1c96a29d45ced66b2fc6cc19edff
BarziBahadin <relaberzy@gmail.com>
2023-09-17 11:53:44 +0300

    after ep6

--- img-gallery/src/app/components/ImgContainer.tsx
+++ img-gallery/src/app/components/ImgContainer.tsx
@@ -8,13 +8,14 @@ type Props = {
 
 export default function ImgContainer({photo}:Props) {
   return (
-    <div  className="h-64 bg-gray-200 rounded-xl">
+    <div  className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
     <Image
         key={photo.id}
         src={photo.src.large}
         alt={photo.alt}
-        width={250}
-        height={250}
+        fill = {true}
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        className="object-cover group-hover:opacity-75"
         />
      </div>
   )
