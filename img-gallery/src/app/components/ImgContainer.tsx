30d5412601c3ab5c49af72ad268ff2bc66a9ac3c
BarziBahadin <relaberzy@gmail.com>
2023-09-17 15:46:22 +0300

    after ep9 full size images, like to original

--- img-gallery/src/app/components/ImgContainer.tsx
+++ img-gallery/src/app/components/ImgContainer.tsx
@@ -1,24 +1,33 @@
 import type { Photo } from "@/models/Images";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-    photo:Photo
-}
+  photo: Photo;
+};
 
-
-export default function ImgContainer({photo}:Props) {
+export default function ImgContainer({ photo }: Props) {
+  const widthHeightRatio = photo.height / photo.width;
+  const galleryHeight = Math.ceil(250 * widthHeightRatio);
+  const photoSpans = Math.ceil(galleryHeight / 10) + 1;
   return (
-    <div  className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
-    <Image
+    <div className="w-[250] justify-center"
+    style={{gridRow: `span ${photoSpans}`}}
+    >
+      <Link href={photo.url} target="_blank" className="grid place-content-center">
+     
+      <div className="rounded-xl overflow-hidden group">
+      <Image
         key={photo.id}
         src={photo.src.large}
         alt={photo.alt}
-        fill = {true}
-        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        width={250}
+        height={galleryHeight}
+        sizes="250px"
         placeholder="blur"
         blurDataURL={photo.blurredDataUrl}
-        className="object-cover group-hover:opacity-75"
-        />
-     </div>
-  )
+        className="group-hover:opacity-75"
+      />
+    </div> </Link></div>
+  );
 }
